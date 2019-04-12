c612462da47bc714c31181d3fba4ec5c9e6b1831
wangyibin <wangyibin0622@126.com>
2019-04-12 10:33:57 +0800

    0403

--- src/main/java/cn/wyb/personal/config/MyBatisConfig.java
+++ src/main/java/cn/wyb/personal/config/MyBatisConfig.java
@@ -3,7 +3,7 @@ package cn.wyb.personal.config;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * ValidatorConfig : 参数校验配置.
+ * MyBatis配置
  *
  * @author : wangyibin
  * @date : 2019/3/23 10:25
